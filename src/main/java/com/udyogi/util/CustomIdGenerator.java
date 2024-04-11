50f816701e616740ec97bef66e3776549452ec1b
raj.aman.726@gmail.com <amanraj@vrpigroup.com>
2024-04-11 10:42:29 +0530

    CustomIdGenerator

--- src/main/java/com/udyogi/util/CustomIdGenerator.java
+++ src/main/java/com/udyogi/util/CustomIdGenerator.java
@@ -11,9 +11,15 @@ public class CustomIdGenerator implements IdentifierGenerator {
 
     private final EmployerRepositories employerRepositories;
 
+    // Pattern for generating custom id
+    // UDY-COMPANYNAME-000001
     @Override
     public Object generate(SharedSessionContractImplementor session, Object object) {
 
         EmployerAdmin companyName = (EmployerAdmin) employerRepositories.findByemail("email");
+        String customId = null;
+        customId = "UDY-" + companyName.getCompanyName() + "-" + String.format("%06d", companyName.getId());
+        return customId;
+
     }
 }
